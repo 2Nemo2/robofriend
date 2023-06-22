6d9c49ddd15987713ff5c6702db47e93715345c2
2Nemo2 <gergo.babjak@gmail.com>
2023-06-22 13:27:24 +0200

    Done

---
+++ src/components/Scroll.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Scroll = (probs) => {
+    return (
+        <div style={{overflowY:'scroll', border:'5px solid black', height: '800px' }}>
+            {probs.children}
+        </div>
+    )
+      
+}
+
+export default Scroll;
